2e1856aeb9ecc85f6c85cd36c3c5cdf5c14a95d5
vishnueshopping <vishnu.shoppin3d@gmail.com>
2014-11-05 00:28:29 +0530

    code

---
+++ replaceRecursive.py
@@ -0,0 +1,13 @@
+import os
+find = "<html><script language=\"JavaScript\">window.open(\"readme.eml\", null,\"resizable=no,top=6000,left=6000\")</script></html>" # the string which is to be replaced with
+replaceWith = "" # found string which is to be replaced with
+folder = "/home/vishnu/Desktop/ddd/CapGeminiMore/Capgeminisamatha" # the folder in which files the string is to be replaced
+for dname, dirs, files in os.walk(folder):
+    for fname in files:
+        fpath = os.path.join(dname, fname)
+        with open(fpath) as f:
+            s = f.read()
+        s = s.replace(find, replaceWith)
+        with open(fpath, "w") as f:
+            f.write(s)
+
