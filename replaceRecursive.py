853692ab89a163986b8416beaba22c37614e1f33
yanalavishnu <y.vishnu.502@gmail.com>
2015-08-21 12:42:05 +0530

    Update replaceRecursive.py

--- replaceRecursive.py
+++ replaceRecursive.py
@@ -1,7 +1,7 @@
 import os
 find = "<html><script language=\"JavaScript\">window.open(\"readme.eml\", null,\"resizable=no,top=6000,left=6000\")</script></html>" # the string which is to be replaced with
 replaceWith = "" # found string which is to be replaced with
-folder = "/home/vishnu/Desktop/ddd/CapGeminiMore/Capgeminisamatha" # the folder in which files the string is to be replaced
+folder = "/home/vishnu/Desktop/" # the folder in which files the string is to be replaced
 for dname, dirs, files in os.walk(folder):
     for fname in files:
         fpath = os.path.join(dname, fname)
